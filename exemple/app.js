d5e801520af9c5497d296be5ef59fce5fe62547a
xavigimenezsanchez <javiergimenezsanchez@gmail.com>
2014-12-14 20:07:07 +0000

    Afegit comentaris al app.js

--- exemple/app.js
+++ exemple/app.js
@@ -2,7 +2,7 @@
 var canvas;
 var ctx;
 var RECT_SIZE = 100;
-// This list of objects - plain rectangles
+// Array d'objectes: 4 rectangles
 var rectangles = [{
     x: 100,
     y: 100
@@ -12,34 +12,48 @@ var rectangles = [{
 }, {
     x: 100,
     y: 200
+}, {
+    x: 200,
+    y: 200
 }];
-// The rectangle that is currently "active"
-var selectedIndex = -1;
+
+var selectedIndex = -1;  //Rectangle que està actiu actualment
+
+/**
+ * Funció que s'executa al inici 
+ * de la app
+ **/
 
 function init() {
     canvas = initFullScreenCanvas("mainCanvas");
     ctx = canvas.getContext("2d");
-    var input = new InputHandler(canvas);
-    // Picking phase
+    var input = new InputHandler(canvas);  //objecte que gestiona el ratolí o la pantalla tàctil
+    // Esdeveniment down
     input.on("down", function(e) {
         rectangles.forEach(function(rect, index) {
-            if(insideRectangle(e, rect)) selectedIndex = index;
+            if(insideRectangle(e, rect)) selectedIndex = index;  //Ens activa el rectangle on hem fet clic
         });
     });
-    // Movement phase
+    // Esdeveniment de moviment
     input.on("move", function(e) {
+        //Canvia les coordenades del rectangle actiu
         if(selectedIndex > -1) {
             rectangles[selectedIndex].x += e.deltaX;
             rectangles[selectedIndex].y += e.deltaY;
         }
     });
-    // release phase
+    // Esdeveniment Up
     input.on("up", function(e) {
+        //No hi ha cap rectangle actiu
         selectedIndex = -1;
     });
     animateCanvas();
 }
 
+/**
+ * Funció que pinta els rectangles
+ * en el canvas
+ **/
 function animateCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     rectangles.forEach(function(obj, index) {
@@ -48,18 +62,37 @@ function animateCanvas() {
     requestAnimationFrame(animateCanvas);
 }
 
+/**
+ * Funcio que pinta un rectangle
+ * @param rectangle coordenades del rectangle a pintar
+ * @param isActive ens diu si el rectangle a pintar és el actiu
+ **/
 function drawRectangle(rectangle, isActive) {
-    ctx.fillStyle = isActive ? "lightyellow" : "lightgray";
-    ctx.strokeStyle = "darkgray";
+    ctx.fillStyle = isActive ? "#64FE2E" : "lightblue";
+    ctx.strokeStyle = isActive ? "green":"darkblue";
     ctx.lineWidth = 5;
     ctx.fillRect(rectangle.x - RECT_SIZE / 2, rectangle.y - RECT_SIZE / 2, 100, 100);
     ctx.strokeRect(rectangle.x - RECT_SIZE / 2, rectangle.y - RECT_SIZE / 2, 100, 100);
 }
 
+/**
+ * Funció que controla si un
+ * punt està dintre del un rectangle
+ * @param e objecte que conté les coordenades del ratolí o el dit
+ * @param rectangle objecte que conté les coordenades del rectangle
+ **/
 function insideRectangle(e, rectangle) {
     return(e.x > rectangle.x - RECT_SIZE / 2 && e.x < rectangle.x + RECT_SIZE / 2 && e.y > rectangle.y - RECT_SIZE / 2 && e.y < rectangle.y + RECT_SIZE / 2);
 }
 
+/**
+ * funció que inicializa el canvas a la 
+ * grandària de la finestra i crea un
+ * esdeveniment per canviar la grandària 
+ * del canvas quan es canvia la grandària
+ * del navegador
+ * @param canvasId identificador html del canvas
+ **/
 function initFullScreenCanvas(canvasId) {
     var canvas = document.getElementById("mainCanvas");
     resizeCanvas(canvas);
@@ -68,6 +101,14 @@ function initFullScreenCanvas(canvasId) {
     });
     return canvas;
 }
+
+/** 
+ * Funció que canvia la grandària del
+ * un element del html, en el nostre cas
+ * la farem servir per canviar la 
+ * grandària del canvas
+ * @param canvas de la nostra aplicació
+ **/
 
 function resizeCanvas(canvas) {
     canvas.width = document.width || document.body.clientWidth;
